ea49f5b9fcb960fd7e43bd1785d0a65ce7aed380
unknown <dariofacundodasilva@gmail.com>
2019-11-13 23:25:02 -0300

    correccion properties en config.js

--- src/services/express/index.js
+++ src/services/express/index.js
@@ -8,9 +8,6 @@ import { errorHandler as bodyErrorHandler } from 'bodymen'
 import { env, fileHost } from '../../config'
 import fileUpload from 'express-fileupload'
 
-const http = require('http');
-const https = require('https');
-const request = require('request');
 var rp = require('request-promise');
 
 export default (apiRoot, routes) => {
@@ -35,9 +32,8 @@ export default (apiRoot, routes) => {
     if (!req.files || Object.keys(req.files).length === 0) {
       return res.status(400).send('No files were uploaded.');
     }
-    console.log("req.files",req.files);
+    
     let sampleFile = req.files.file;
-    console.log("sampleFile",sampleFile);
 
     var options = {
       method: 'POST',
@@ -58,12 +54,15 @@ export default (apiRoot, routes) => {
 
     rp(options)
     .then(function (body) {
-      console.log("body",body);
-      return res.status(200).send(body);
+      var result = JSON.parse(body);
+      var url="";
+      if(result != null && result.variations != null && result.variations.length > 0){
+        url = result.variations[0].url;
+      }
+      return res.status(200).send({url:url});
     })
     .catch(function (err) {
-      console.log("err",err);
-      return res.status(500).send(err);
+      return res.status(500).send("No se pudo cargar la imagen al sistema.");
     });
 
   });

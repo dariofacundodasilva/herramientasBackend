cf8b73338d407d1e6000acfd1f4be95b12c80874
unknown <dariofacundodasilva@gmail.com>
2019-11-29 17:04:28 -0300

    calculo promedio mejorado

--- src/api/alquiler/model.js
+++ src/api/alquiler/model.js
@@ -18,11 +18,18 @@ const alquilerSchema = new Schema({
 
 alquilerSchema.methods = {
   view (full) {
+
+    
+    calcularPromedioReputacionHerramienta(this.herramienta);
+    calcularPromedioReputacionCliente(this.cliente);
     const view = {
       id: this.id,
       cliente: this.cliente,
+      promedioCliente:calcularPromedioReputacionCliente(this.cliente),
       herramienta: this.herramienta,
+      promedioHerramienta:calcularPromedioReputacionHerramienta(this.herramienta),
       proveedor: this.proveedor,
+      promedioProveedor:calcularPromedioReputacionProveedor(this.proveedor),
       monto: this.monto,
       cantidad:this.cantidad,
       dias: this.dias,
@@ -37,6 +44,46 @@ alquilerSchema.methods = {
     } : view
   }
 }
+
+var calcularPromedioReputacionProveedor= function(proveedor){
+  var promedio= 0;
+  var reputacion = proveedor.reputacionProveedor;
+  if(reputacion && reputacion.length > 0){
+    reputacion.map((rep)=>{
+      promedio = promedio + rep.puntaje;
+    });
+    promedio = promedio / reputacion.length;
+  }
+  
+  return  promedio;
+}
+
+var calcularPromedioReputacionHerramienta= function(herramienta){
+  var promedio= 0;
+  var reputacion = herramienta.reputacion;
+  if(reputacion && reputacion.length > 0){
+    reputacion.map((rep)=>{
+      promedio = promedio + rep.puntaje;
+    });
+    promedio = promedio / reputacion.length;
+  }
+  
+  return  promedio;
+}
+
+var calcularPromedioReputacionCliente= function(cliente){
+  var promedio= 0;
+  var reputacion = cliente.reputacionUsuario;
+  if(reputacion && reputacion.length > 0){
+    reputacion.map((rep)=>{
+      promedio = promedio + rep.puntaje;
+    });
+    promedio = promedio / reputacion.length;
+  }
+  
+  return  promedio;
+}
+
 
 
 const model = mongoose.model('Alquiler', alquilerSchema)
